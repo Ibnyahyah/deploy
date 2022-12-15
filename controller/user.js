1a5dd90fbf7a8dd0686bc3a3589031935c6c2b30
ibnyahyah <yahyahridwan665@gmail.com>
2022-12-15 21:51:21 +0100

    update

--- controller/user.js
+++ controller/user.js
@@ -58,12 +58,13 @@ const adminSignIn = async (req, res) => {
 }
 
 const updateAdmin = async (req, res) => {
+    const { id } = req.params;
     const { email, name, phone, role } = req.body;
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decoded = JWT.verify(token, process.env.ACCESS_TOKEN_SECRET);
         if (decoded.data.role !== 'admin') return res.status(401).json({ message: 'unauthorized' });
-        const user = await User.findOneAndUpdate({ email });
+        const user = await User.findByIdAndUpdate(id);
         if (!user) return res.status(401).json({ message: 'User not found' });
         user.name = name;
         user.email = email;
--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/register', (req, res) => registerAdmin(req, res));
 router.post('/login', (req, res) => adminSignIn(req, res));
-router.patch('/', (req, res) => updateAdmin(req, res));
+router.patch('/:id', (req, res) => updateAdmin(req, res));
 router.get('/get-all-admins', (req, res) => getAllAdmins(req, res));
 router.get('/get-admin/:id', (req, res) => getAdmin(req, res));
 router.delete('/delete-admin/:id', (req, res) => deleteAdmin(req, res));

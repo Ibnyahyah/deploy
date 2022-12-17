f23876f0293a76ce7a9c87467a701a1127af50ac
ibnyahyah <yahyahridwan665@gmail.com>
2022-12-17 12:41:30 +0100

    update

--- controller/analytics.js
+++ controller/analytics.js
@@ -6,6 +6,33 @@ const Agent = require('../model/agent');
 
 const JWT = require('jsonwebtoken')
 
+const arrProd = [
+    {
+        title: 'post 11-3-2022',
+        date: '11-3-2022',
+    },
+    {
+        title: 'post 12-1-2022',
+        date: '12-1-2022',
+    },
+    {
+        title: 'post 12-4-2022',
+        date: '12-4-2022',
+    },
+    {
+        title: 'post 12-6-2022',
+        date: '12-6-2022',
+    },
+    {
+        title: 'post 11-20-2022',
+        date: '11-20-2022',
+    },
+    {
+        title: 'post 11-30-2022',
+        date: '11-30-2022',
+    },
+]
+
 
 const getAnalytics = async (req, res) => {
     const { date } = req.params;
@@ -24,7 +51,7 @@ const getAnalytics = async (req, res) => {
         const _toMonth = new Date(toDate).getMonth() < 10 ? '0' + new Date(toDate).getMonth() : new Date(toDate).getMonth()
 
 
-        console.log('from date', _fromDate, 'todate', _toDate, 'from month', _fromMonth, 'to month', _toMonth)
+        // console.log('from date', _fromDate, 'todate', _toDate, 'from month', _fromMonth, 'to month', _toMonth)
         const token = req.headers.authorization.split(" ")[1];
         if (!token) return res.status(401).json({ message: 'unauthorized' });
         const decoded = JWT.verify(token, process.env.ACCESS_TOKEN_SECRET);
@@ -32,39 +59,63 @@ const getAnalytics = async (req, res) => {
             const orders = await Order.find();
             const customers = await Customer.find();
 
-            const ords = [];
-            const cutms = [];
+            let OrdersArray = [];
+            let customersArray = [];
 
             // console.log(new Date(fromDate).getDay());
             // const dateChecker = (a, b, c) => !toDate ? a == todayDate : (b > _fromDate && b > _toDate ? ((b <= _fromDate || b >= _toDate) && (c >= _fromMonth && c <= _toMonth)) : (((b <= _fromDate && b >= _toDate) || (b >= _fromDate && b <= _toDate)) && (c >= _fromMonth && c <= _toMonth)));
             // const dateChecker = (a, b, c) => !toDate ? a == todayDate : ((b >= _fromDate || b <= _toDate || (b <= _fromDate || b >= _toDate)) && (b >= _fromDate || b <= _toDate)) && (c >= _fromMonth && c <= _toMonth);
-            // const dateChecker = (a, b, c) => toDate == undefined ? a == todayDate : ((b >= _fromDate && b >= _toDate) || b <= _toDate ? (((b >= _fromDate || b <= _fromDate) && (b >= _toDate || b <= _toDate) || (b >= _fromDate && b <= _toDate)) && (c >= _fromMonth && c <= _toMonth)) : ((b >= _fromDate && b <= _toDate) && (c >= _fromMonth && c <= _toMonth)));
-            const dateChecker = (a, b, c) => toDate == undefined ? a == todayDate : b >= _fromDate && b <= _toDate && c >= _fromMonth && c <= _toMonth;
+            const dateChecker = (a, b, c) => toDate == undefined ? a == todayDate : ((b >= _fromDate && b >= _toDate) || b <= _toDate ? (((b >= _fromDate || b <= _fromDate) && (b >= _toDate || b >= _toDate)) && (c >= _fromMonth && c <= _toMonth)) : ((b >= _fromDate && b <= _toDate) && (c >= _fromMonth && c <= _toMonth)));
+            // const dateChecker = (a, b, c) => toDate == undefined ? a == todayDate : b >= _fromDate && b <= _toDate && c >= _fromMonth && c <= _toMonth;
 
-            orders.find(function (value) {
-                const prodDate = new Date(value.createdAt).getFullYear() + ':' + new Date(value.createdAt).getMonth() + ':' + new Date(value.createdAt).getDate()
-                const prdDate = new Date(value.createdAt).getDate() < 10 ? '0' + new Date(value.createdAt).getDate() : new Date(value.createdAt).getDate()
-                const prdMonth = new Date(value.createdAt).getMonth() < 10 ? '0' + new Date(value.createdAt).getMonth() : new Date(value.createdAt).getMonth()
-                console.log(dateChecker(prodDate, prdDate, prdMonth));
-                console.log({ 'date': prdDate, 'month': prdMonth }, 'orders');
-                if (dateChecker(prodDate, prdDate, prdMonth) && value.status.toLowerCase() == 'delivered') {
-                    ords.push(value);
-                    console.log(ords)
+            const OrderFilterArray = (arr) => {
+                const res = arr.filter(myFunction);
+                function myFunction(value) {
+                    const valueToday = new Date(value.createdAt).getFullYear() + ':' + new Date(value.createdAt).getMonth() + ':' + new Date(value.createdAt).getDate();
+                    return toDate == undefined ? valueToday == todayDate : (new Date(value.createdAt).getDate() >= _fromDate || new Date(value.createdAt).getDate() >= _toDate) && (new Date(value.createdAt).getMonth() >= _fromMonth && new Date(value.createdAt).getMonth() <= _toMonth) && value.status.toLowerCase() == 'delivered';
                 }
-            });
-            customers.find(function (value) {
-                const customerDate = new Date(value.createdAt).getFullYear() + ':' + new Date(value.createdAt).getMonth() + ':' + new Date(value.createdAt).getDate()
-                const cusDate = new Date(value.createdAt).getDate() < 10 ? '0' + new Date(value.createdAt).getDate() : new Date(value.createdAt).getDate()
-                const cusMonth = new Date(value.createdAt).getMonth() < 10 ? '0' + new Date(value.createdAt).getMonth() : new Date(value.createdAt).getMonth()
-                console.log(dateChecker(customerDate, cusDate, cusMonth));
-                console.log({ 'date': cusDate, 'month': cusMonth }, 'customer');
-                if (dateChecker(customerDate, cusDate, cusMonth)) {
-                    cutms.push(value);
+                return res;
+            }
+            const CustomerFilterArray = (arr) => {
+                const result = arr.filter(myFunction);
+                function myFunction(value) {
+                    const valueToday = new Date(value.createdAt).getFullYear() + ':' + new Date(value.createdAt).getMonth() + ':' + new Date(value.createdAt).getDate();
+                    return toDate == undefined ? valueToday == todayDate : (new Date(value.createdAt).getDate() >= _fromDate || new Date(value.createdAt).getDate() >= _toDate) && (new Date(value.createdAt).getMonth() >= _fromMonth && new Date(value.createdAt).getMonth() <= _toMonth);
                 }
-            });
+                return result;
+            }
+
+            const orderValue = OrderFilterArray(orders);
+            OrdersArray = orderValue;
+            const customerValue = CustomerFilterArray(customers);
+            customersArray = customerValue;
+
+            console.log(OrdersArray);
+            // orders.find(function (value) {
+            //     const prodDate = new Date(value.createdAt).getFullYear() + ':' + new Date(value.createdAt).getMonth() + ':' + new Date(value.createdAt).getDate()
+            //     const prdDate = new Date(value.createdAt).getDate() < 10 ? '0' + new Date(value.createdAt).getDate() : new Date(value.createdAt).getDate()
+            //     const prdMonth = new Date(value.createdAt).getMonth() < 10 ? '0' + new Date(value.createdAt).getMonth() : new Date(value.createdAt).getMonth()
+            //     // console.log(dateChecker(prodDate, prdDate, prdMonth));
+            //     // console.log({ 'date': prdDate, 'month': prdMonth }, 'orders');
+            //     // console.log(value, 'orders');
+            //     if (dateChecker(prodDate, prdDate, prdMonth) && value.status.toLowerCase() == 'delivered') {
+            //         ords.push(value);
+            //         console.log(ords)
+            //     }
+            // });
+            // customers.find(function (value) {
+            //     const customerDate = new Date(value.createdAt).getFullYear() + ':' + new Date(value.createdAt).getMonth() + ':' + new Date(value.createdAt).getDate()
+            //     const cusDate = new Date(value.createdAt).getDate() < 10 ? '0' + new Date(value.createdAt).getDate() : new Date(value.createdAt).getDate()
+            //     const cusMonth = new Date(value.createdAt).getMonth() < 10 ? '0' + new Date(value.createdAt).getMonth() : new Date(value.createdAt).getMonth()
+            //     // console.log(dateChecker(customerDate, cusDate, cusMonth));
+            //     // console.log({ 'date': cusDate, 'month': cusMonth }, 'customer');
+            //     if (dateChecker(customerDate, cusDate, cusMonth)) {
+            //         cutms.push(value);
+            //     }
+            // });
             let _amountOfSales = 0;
             let _noOfBagSold = 0;
-            ords.forEach(function ({ products, orderTotalPrice }) {
+            OrdersArray.forEach(function ({ products, orderTotalPrice }) {
                 let total = 0;
                 products.forEach(function (value) {
                     _noOfBagSold += value.orderQuantity;
@@ -74,7 +125,7 @@ const getAnalytics = async (req, res) => {
             });
 
 
-            res.status(200).json({ date: date, amountOfSales: _amountOfSales, noOfBagSold: _noOfBagSold, noOfActiveCustomers: '0', noOfRegisteredCustomers: cutms.length, noOfOrders: ords.length });
+            res.status(200).json({ date: date, amountOfSales: _amountOfSales, noOfBagSold: _noOfBagSold, noOfActiveCustomers: '0', noOfRegisteredCustomers: customersArray.length, noOfOrders: OrdersArray.length });
             // res.status(200).json({ ords: ords });
         } else { return res.status(401).json({ message: 'unauthorized' }); }
     } catch (err) {
